ee097c5978b11c67957674a123b3cfbcbd3073cf
Rahat-Ul-Anwar <anwarrahat69@gmail.com>
2022-11-25 17:20:39 +0600

    from validation

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,10 +1,18 @@
 
 import React from "react";
+import { useForm } from "react-hook-form";
 
 import { Link } from "react-router-dom";
 
 
 const Login = () => {
+
+  const { register, handleSubmit,formState: { errors } } = useForm();
+
+
+  const handleLogin = (data) => {
+    console.log(data);
+  }
 
 return (
     <div className="hero">
@@ -12,30 +20,32 @@ return (
       
         <div className="card flex-shrink-0 w-full shadow-2xl py-10 bg-base-100">
           <h1 className="text-4xl text-center font-bold">Login</h1>
-          <form  className="card-body">
+          <form onSubmit={handleSubmit(handleLogin)} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
-              <input
-                type="text"
-                placeholder="email"
-                name="email"
-                className="input input-bordered"
-                required
-              />
+            <input type="text" placeholder="email" className="input input-bordered"
+              {...register("email", {
+                required: 'Email address is required'
+              
+              })} 
+                
+            />
+            {errors?.email && <p className="text-error">{errors.email?.message}</p>}
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
-              <input
-                type="text"
-                placeholder="password"
-                name="password"
-                className="input input-bordered"
-                required
-              />
+            <input type="password" placeholder="password" className="input input-bordered"
+              {...register("password", {
+                required: 'password is required',
+                minLength: {value:6, message: 'password must be 6 characters longer'}
+              })}
+               
+            />
+                    {errors?.password && <p className="text-error">{errors.password?.message}</p>}
               <label className="label">
                 <Link href="#" className="label-text-alt link link-hover">
                   Forgot password?
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,63 +1,73 @@
+
 import React from "react";
+import { useForm } from "react-hook-form";
+
 import { Link } from "react-router-dom";
 
+
 const Register = () => {
-  
-  return (
+
+  const { register, handleSubmit,formState: { errors } } = useForm();
+
+
+  const handleLogin = (data) => {
+    console.log(data);
+  }
+
+return (
     <div className="hero">
       <div className="hero-content ">
-       
-        <div className="card flex-shrink-0 w-full  shadow-2xl py-10 bg-base-100">
+      
+        <div className="card flex-shrink-0 w-full shadow-2xl py-10 bg-base-100">
           <h1 className="text-4xl text-center font-bold">Sign Up</h1>
-          <form  className="card-body">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Name</span>
-              </label>
-              <input
-                type="text"
-                placeholder="Your Name"
-                name="name"
-                className="input input-bordered"
-                required
-              />
-            </div>
+          <form onSubmit={handleSubmit(handleLogin)} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
-              <input
-                type="text"
-                placeholder="email"
-                name="email"
-                className="input input-bordered"
-                required
-              />
+            <input type="text" placeholder="email" className="input input-bordered"
+              {...register("email", {
+                required: 'Email address is required'
+              
+              })} 
+                
+            />
+            {errors?.email && <p className="text-error">{errors.email?.message}</p>}
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
-              <input
-                type="text"
-                placeholder="password"
-                name="password"
-                className="input input-bordered"
-                required
-              />
+            <input type="password" placeholder="password" className="input input-bordered"
+              {...register("password", {
+                required: 'password is required',
+                minLength: {value:6, message: 'password must be 6 characters longer'}
+              })}
+               
+            />
+                    {errors?.password && <p className="text-error">{errors.password?.message}</p>}
+              <label className="label">
+                <Link href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </Link>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <input className="btn btn-primary" type="submit" value="Login" />
             </div>
             <div className="form-control mt-6">
               <input
-                className="btn btn-primary"
+             
+                className="btn btn-secondary"
                 type="submit"
-                value="Sign Up"
+                value="Continue with Google"
               />
             </div>
           </form>
           <p className="text-center">
-            Already have an account? please,
+          All ready have an account?
             <Link className="text-orange-600 font-semibold" to="/login">
-              Login
+              Log In
             </Link>
           </p>
         </div>
